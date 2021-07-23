a638e8f6e68fd87ff9e48723ddab9d1ba8d60f9b
AP <ashis.n.patel@gmail.com>
2021-07-22 20:40:44 -0500

    Add clear all tasks button and style for textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,52 @@
 // Create empty object to hold the tasks
 let tasks = {};
+// startTime and endTime both in 24-hour clock
+let startTime = 9 // hour that workday starts and first timeslot
+let endTime = 18 // hour AFTER the last hour of the workday, if the workday ends at 17 (5PM), set endTime to 18 (6PM) 
 // Add ability to click and edit tasks
 
 // Select the paragraph element in the task wrapper 
 $(".task-wrapper").on("click", function () {
-    console.log("You clicked on a task!");
     // Select paragraph that is a child of the task div and its text content
     let task = $(this).children("p");
     let text = task.text().trim();
     // Create text input area to enter new task 
-    let textInput = $("<textarea>").addClass("task-text d-inline position-absolute border border-danger").val(text);
+    let textInput = $("<textarea>").addClass("task-text d-inline position-absolute").val(text);
     // Replace p element with textinput 
     task.replaceWith(textInput);
     // Make the cursor select the textinput 
     textInput.trigger("focus");
-
-
-    // let task = $(this).children("p"); 
-    // task.attr("contenteditable",true); 
 })
-
-
 $(".task-wrapper").on("blur", "textarea", function () {
-    console.log("This will revert the textarea")
     // Grab task text
     const taskText = $(this).val().trim();
     // Recreate p tag
     let taskP = $("<p>").addClass("task-p").text(taskText);
     // Replace textarea with p
     $(this).replaceWith(taskP);
-
 })
-
 // Add ability to save tasks in local storage
 $(".time-row").on("click", ".save-task", function () {
-    checkId = $(this).attr("id");
-    console.log(checkId);
     // Grab timeslot that needs its task saved in the tasks array 
     const timeSlot = $(this).attr("id").replace("save", "task");
     // Grab text from the timeslot paragraph
     const taskText = $(this).closest(".row").find(".task-p").text().trim();
-    // Update task array 
+    // Update tasks object and save 
     tasks[timeSlot] = taskText;
-    console.log(tasks);
     saveTasks();
+    // Find the time for this task
+    const taskTime = $(this).closest(".row").find(".time-wrap p").text().trim();
+    // Tell the user that the task for the timeRow where the button was clicked has been saved 
+    alert(`The task for ${taskTime} has been saved!`);
 })
-
 // Save task function 
 const saveTasks = function () {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
-
 // Load task function 
 const loadTasks = function () {
     tasks = JSON.parse(localStorage.getItem("tasks"));
-    // if there are no tasks skip
+    // if there are no tasks, populate objects with the right keys and empty values
     if (!tasks) {
         tasks = { tasks9: "", tasks10: "", task11: "", task12: "", task1: "", task2: "", task3: "", task4: "", task5: "" };
     }
@@ -62,21 +54,19 @@ const loadTasks = function () {
     for (const task in tasks) {
         $("#" + task).find(".task-p").text(tasks[task]);
     }
+    // Check background color of tasks upon loading webpage
+    taskCheck();
 }
-
 // Function to change the color of the tasks background based on the approximate due-time
 const taskCheck = function (task) {
-    // startTime and endTime both in 24-hour clock
-    let startTime = 9 // hour that workday starts and first timeslot
-    let endTime = 18 // hour AFTER the last hour of the workday, if the workday ends at 17 (5PM), set endTime to 18 (6PM) 
     let hour = dayjs().hour();
     for (let i = startTime; i < endTime; i++) {
-        let timeBox = $("#time-" + i).find(".task-wrapper"); 
+        let timeBox = $("#time-" + i).find(".task-wrapper");
         timeBox.removeClass("bg-secondary bg-danger bg-success");
-        if (hour < i ) {
-            timeBox.addClass("bg-success"); 
+        if (hour < i) {
+            timeBox.addClass("bg-success");
         }
-        else if (hour === i ) {
+        else if (hour === i) {
             timeBox.addClass("bg-danger");
         }
         else {
@@ -84,18 +74,39 @@ const taskCheck = function (task) {
         }
     }
 }
+// Function to clear all tasks 
+$("#clear-tasks-btn").on("click", function() {
+    // Confirm if the user wants to delete all tasks
+    let confirm = window.confirm("Are you sure you want to clear all tasks?");
+    if (confirm) {
+        for(let i = startTime; i < endTime; i++) {
+            // Find task for each time slot
+            let task = $("#time-"+i).find(".task-p");
+            // Clear the task text for each time slot
+            task.text(""); 
+            // Grab the timeslot and the taskText to save in the tasks object
+            let timeSlot = $("#time-"+i).find(".task-wrapper").attr("id"); 
+            let taskText = task.text().trim(); 
+            tasks[timeSlot] = taskText; 
+            // Save empty tasks object to localStorage
+            saveTasks(); 
+        }
+    }
+    else {
+        return false;
+    }
+}); 
 
-setInterval(function() {
+// Run taskCheck every 5 minutes to update task background color based on the time 
+setInterval(function () {
     taskCheck();
-    console.log("I've updated!"); 
-}, (1000*60)*5); 
+}, (1000 * 60) * 5);
 
 
 // Change bg-color of div containing tasks based on relation to current
 loadTasks();
 
 // Set current day on page
-const currentDay = dayjs().format("MM/DD/YYYY");
+const currentDay = dayjs().format("dddd, MMMM, YYYY");
 $("#currentDay").text(currentDay);
 
-taskCheck();
